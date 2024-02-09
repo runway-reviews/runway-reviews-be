32418a73516a5df7dde1c66456579645500774a1
Blake Sergesketter <blakesergesketter@gmail.com>
2024-02-09 12:02:49 -0600

    Merge main to branch from the approved/merged pull request.

--- backend_api/views.py
+++ backend_api/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render, HttpResponse
+import requests
 from rest_framework.views import APIView 
 from backend_api.models import User, Review  
 from backend_api.serializers import UserSerializer, ReviewSerializer 
 from rest_framework.response import Response
 from rest_framework import status 
+from django.http import JsonResponse
 
 # User actions 
 class UserDetails(APIView):
@@ -18,9 +20,6 @@ class UserDetails(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-# Review actions  
-class ReviewDetails(APIView):
 # Review actions  
 class ReviewDetails(APIView):
 
@@ -49,14 +48,6 @@ class ReviewDetails(APIView):
         return Response({
           'error': 'Review does not exist' 
           }, status=status.HTTP_404_NOT_FOUND)
-  # set_review
-  def get_review(request, review_id):
-    try: 
-        return Review.objects.get(pk=review_id)
-    except: 
-        return Response({
-          'error': 'Review does not exist' 
-          }, status=status.HTTP_404_NOT_FOUND)
 
   # get review
   def get(self, request, review_id, user_id):
@@ -64,17 +55,7 @@ class ReviewDetails(APIView):
       user = self.get_user(user_id)
       serializer = ReviewSerializer(review, user) # integrating the airport id into the serializer here 
       return Response(serializer.data) 
-  # get review
-  def get(self, request, review_id, user_id):
-      review = self.get_review(review_id)
-      user = self.get_user(user_id)
-      serializer = ReviewSerializer(review, user) # integrating the airport id into the serializer here 
-      return Response(serializer.data) 
 
-  def post(self, request, review_id, user_id):
-      review = self.get_review(review_id)
-      user = self.get_user(user_id)
-      serializer = ReviewSerializer(data=request.data)
   def post(self, request, review_id, user_id):
       review = self.get_review(review_id)
       user = self.get_user(user_id)
@@ -97,4 +78,3 @@ def get_airports(request):
       return JsonResponse(data, safe=False)
   else:
       return JsonResponse({"error": "Failed to fetch data"}, status=500)
-  
