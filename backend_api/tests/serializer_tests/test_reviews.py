b363fa6d9d7c600d6ae03bc4c4fc70bde1178b09
Blake Sergesketter <blakesergesketter@gmail.com>
2024-02-09 15:42:21 -0600

    TEST- Start of adding serializer testing. Committing to switch branches and pull from main.

---
+++ backend_api/tests/serializer_tests/test_reviews.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from rest_framework.serializers import ValidationError
+from serializers import ReviewSerializer
+from backend_api.models import Review, User, Airport # Airport is not migrated which is why it's gray 
+
+class ReviewSerializerTest(TestCase):
+  def test_review_serializer(self):
+    data = {
+      "id": "1",
+      "user_id": "1",
+      "airport_id": "1",
+      "category": "security",
+      "comment": "TSA seemed a little TOO handsy for my liking."
+    }
