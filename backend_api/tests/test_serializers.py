fa216e142906f1efa5de2686c42ec9bad2ecc355
Blake Sergesketter <blakesergesketter@gmail.com>
2024-02-10 11:15:15 -0600

    Merge from main, sort merge conflicts, delete unneeded files and directories.

--- backend_api/tests/test_serializers.py
+++ backend_api/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.serializers import ValidationError
-from serializers import UserSerializer
+from serializers import UserSerializer, ReviewSerializer
+from backend_api.models import Review, User, Airport
 
 class UserSerializerTest(TestCase):
   def test_user_serializer(self):
@@ -24,3 +25,15 @@ class UserSerializerInvalid(TestCase):
     self.assertTrue(serializer.is_valid())
 
     self.assertNotEqual(serializer.errors, {})
+
+
+
+class ReviewSerializerTest(TestCase):
+  def test_review_serializer(self):
+    data = {
+      "id": "1",
+      "user_id": "1",
+      "airport_id": "1",
+      "category": "security",
+      "comment": "TSA seemed a little TOO handsy for my liking."
+    }
--- backend_api/views.py
+++ backend_api/views.py
@@ -10,44 +10,6 @@ from backend_api.serializers import UserSerializer, ReviewSerializer, AirportSer
 
 
 # Review actions  
-class ReviewDetails(APIView):
-
-  # set_user
-  def get_user(request, user_id):
-    try: 
-        return User.objects.get(pk=user_id)
-    except: 
-        return Response({
-          'error': 'User does not exist' 
-          }, status=status.HTTP_404_NOT_FOUND)
-
-  # set_review
-  def get_review(request, review_id):
-    try: 
-        return Review.objects.get(pk=review_id)
-    except: 
-        return Response({
-          'error': 'Review does not exist' 
-          }, status=status.HTTP_404_NOT_FOUND)
-
-  # get review
-  def get(self, request, review_id, user_id):
-      review = self.get_review(review_id)
-      user = self.get_user(user_id)
-      serializer = ReviewSerializer(review, user) # integrating the airport id into the serializer here 
-      return Response(serializer.data) 
-
-  def post(self, request, review_id, user_id):
-      review = self.get_review(review_id)
-      user = self.get_user(user_id)
-      serializer = ReviewSerializer(data=request.data)
-      
-      if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-      else: 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 def get_airports(request):
   url = "https://api.api-ninjas.com/v1/airports?country=us"
   headers = { 
@@ -140,8 +102,3 @@ class ReviewDetails(APIView):
       review = get_object_or_404(Review, pk=review_id)
       review.delete()
       return Response({'message': 'Review deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
-     
-
-
-      
-    
